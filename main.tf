642fc10a61bbcb9a1edb4e5db3417216eeda6ff6
Gurpreetw <preetgur.gsr@gmail.com>
2022-03-10 16:25:55 +0530

    edit main tf file

--- main.tf
+++ main.tf
@@ -16,13 +16,3 @@ resource "azurerm_resource_group" "preetrg" {
   name     = "mypreetrg"
   location = "central india"
 }
-
-resource "azurerm_app_service_plan" "myappsrvc" {
-  name                = "my_tf_aspl"
-  resource_group_name = azurerm_app_service_plan.myappsrvc.name
-  location            = azurerm_app_service_plan.myappsrvc.location
-  sku {
-    tier = "Standard"
-    size = "S1s"
-  }
-}
